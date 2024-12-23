ac0f2982dc8638edd7663f846cdf43ef1a7aaa26
vlowingkloude <126703177+vlowingkloude@users.noreply.github.com>
2024-12-23 12:55:01 +0100

    change char to varchar for polypheny

--- src/main/resources/benchmarks/tpch/ddl-polypheny.sql
+++ src/main/resources/benchmarks/tpch/ddl-polypheny.sql
@@ -9,14 +9,14 @@ DROP TABLE IF EXISTS lineitem;
 
 CREATE TABLE region (
                         r_regionkey integer  NOT NULL,
-                        r_name      char(25) NOT NULL,
+                        r_name      varchar(25) NOT NULL,
                         r_comment   varchar(152),
                         PRIMARY KEY (r_regionkey)
 );
 
 CREATE TABLE nation (
                         n_nationkey integer  NOT NULL,
-                        n_name      char(25) NOT NULL,
+                        n_name      varchar(25) NOT NULL,
                         n_regionkey integer  NOT NULL,
                         n_comment   varchar(152),
                         PRIMARY KEY (n_nationkey),
@@ -26,11 +26,11 @@ CREATE TABLE nation (
 CREATE TABLE part (
                       p_partkey     integer        NOT NULL,
                       p_name        varchar(55)    NOT NULL,
-                      p_mfgr        char(25)       NOT NULL,
-                      p_brand       char(10)       NOT NULL,
+                      p_mfgr        varchar(25)       NOT NULL,
+                      p_brand       varchar(10)       NOT NULL,
                       p_type        varchar(25)    NOT NULL,
                       p_size        integer        NOT NULL,
-                      p_container   char(10)       NOT NULL,
+                      p_container   varchar(10)       NOT NULL,
                       p_retailprice decimal(15, 2) NOT NULL,
                       p_comment     varchar(23)    NOT NULL,
                       PRIMARY KEY (p_partkey)
@@ -38,10 +38,10 @@ CREATE TABLE part (
 
 CREATE TABLE supplier (
                           s_suppkey   integer        NOT NULL,
-                          s_name      char(25)       NOT NULL,
+                          s_name      varchar(25)       NOT NULL,
                           s_address   varchar(40)    NOT NULL,
                           s_nationkey integer        NOT NULL,
-                          s_phone     char(15)       NOT NULL,
+                          s_phone     varchar(15)       NOT NULL,
                           s_acctbal   decimal(15, 2) NOT NULL,
                           s_comment   varchar(101)   NOT NULL,
                           PRIMARY KEY (s_suppkey),
@@ -64,9 +64,9 @@ CREATE TABLE customer (
                           c_name       varchar(25)    NOT NULL,
                           c_address    varchar(40)    NOT NULL,
                           c_nationkey  integer        NOT NULL,
-                          c_phone      char(15)       NOT NULL,
+                          c_phone      varchar(15)       NOT NULL,
                           c_acctbal    decimal(15, 2) NOT NULL,
-                          c_mktsegment char(10)       NOT NULL,
+                          c_mktsegment varchar(10)       NOT NULL,
                           c_comment    varchar(117)   NOT NULL,
                           PRIMARY KEY (c_custkey),
                           FOREIGN KEY (c_nationkey) REFERENCES nation (n_nationkey) ON DELETE CASCADE
@@ -75,11 +75,11 @@ CREATE TABLE customer (
 CREATE TABLE orders (
                         o_orderkey      integer        NOT NULL,
                         o_custkey       integer        NOT NULL,
-                        o_orderstatus   char(1)        NOT NULL,
+                        o_orderstatus   varchar(1)        NOT NULL,
                         o_totalprice    decimal(15, 2) NOT NULL,
                         o_orderdate     date           NOT NULL,
-                        o_orderpriority char(15)       NOT NULL,
-                        o_clerk         char(15)       NOT NULL,
+                        o_orderpriority varchar(15)       NOT NULL,
+                        o_clerk         varchar(15)       NOT NULL,
                         o_shippriority  integer        NOT NULL,
                         o_comment       varchar(79)    NOT NULL,
                         PRIMARY KEY (o_orderkey),
@@ -95,13 +95,13 @@ CREATE TABLE lineitem (
                           l_extendedprice decimal(15, 2) NOT NULL,
                           l_discount      decimal(15, 2) NOT NULL,
                           l_tax           decimal(15, 2) NOT NULL,
-                          l_returnflag    char(1)        NOT NULL,
-                          l_linestatus    char(1)        NOT NULL,
+                          l_returnflag    varchar(1)        NOT NULL,
+                          l_linestatus    varchar(1)        NOT NULL,
                           l_shipdate      date           NOT NULL,
                           l_commitdate    date           NOT NULL,
                           l_receiptdate   date           NOT NULL,
-                          l_shipinstruct  char(25)       NOT NULL,
-                          l_shipmode      char(10)       NOT NULL,
+                          l_shipinstruct  varchar(25)       NOT NULL,
+                          l_shipmode      varchar(10)       NOT NULL,
                           l_comment       varchar(44)    NOT NULL,
                           PRIMARY KEY (l_orderkey, l_linenumber),
                           FOREIGN KEY (l_orderkey) REFERENCES orders (o_orderkey) ON DELETE CASCADE,
