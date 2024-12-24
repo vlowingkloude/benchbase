eaafc50977c22edda18ead58043aeef70c17da68
vlowingkloude <126703177+vlowingkloude@users.noreply.github.com>
2024-12-24 11:02:37 +0100

    switching to polypheny oltpbench ddls

--- src/main/resources/benchmarks/tpch/ddl-polypheny.sql
+++ src/main/resources/benchmarks/tpch/ddl-polypheny.sql
@@ -7,103 +7,96 @@ DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS lineitem;
 
-CREATE TABLE region (
-                        r_regionkey integer  NOT NULL,
-                        r_name      varchar(25) NOT NULL,
-                        r_comment   varchar(152),
-                        PRIMARY KEY (r_regionkey)
-);
+-- Sccsid:     @(#)dss.ddl  2.1.8.1
+CREATE TABLE nation  ( n_nationkey  INTEGER NOT NULL,
+                       n_name       VARCHAR(25) NOT NULL,
+                       n_regionkey  INTEGER NOT NULL,
+                       n_comment    VARCHAR(152),
+                       PRIMARY KEY (n_nationkey) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE nation ADD INDEX IDX_NATION_PK ON (n_nationkey);
 
-CREATE TABLE nation (
-                        n_nationkey integer  NOT NULL,
-                        n_name      varchar(25) NOT NULL,
-                        n_regionkey integer  NOT NULL,
-                        n_comment   varchar(152),
-                        PRIMARY KEY (n_nationkey),
-                        FOREIGN KEY (n_regionkey) REFERENCES region (r_regionkey) ON DELETE CASCADE
-);
+CREATE TABLE region  ( r_regionkey  INTEGER NOT NULL,
+                       r_name       VARCHAR(25) NOT NULL,
+                       r_comment    VARCHAR(152),
+                       PRIMARY KEY (r_regionkey) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE region ADD INDEX IDX_REGION_PK ON (r_regionkey);
 
-CREATE TABLE part (
-                      p_partkey     integer        NOT NULL,
-                      p_name        varchar(55)    NOT NULL,
-                      p_mfgr        varchar(25)       NOT NULL,
-                      p_brand       varchar(10)       NOT NULL,
-                      p_type        varchar(25)    NOT NULL,
-                      p_size        integer        NOT NULL,
-                      p_container   varchar(10)       NOT NULL,
-                      p_retailprice decimal(15, 2) NOT NULL,
-                      p_comment     varchar(23)    NOT NULL,
-                      PRIMARY KEY (p_partkey)
-);
+CREATE TABLE part  ( p_partkey     INTEGER NOT NULL,
+                     p_name        VARCHAR(55) NOT NULL,
+                     p_mfgr        VARCHAR(25) NOT NULL,
+                     p_brand       VARCHAR(10) NOT NULL,
+                     p_type        VARCHAR(25) NOT NULL,
+                     p_size        INTEGER NOT NULL,
+                     p_container   VARCHAR(10) NOT NULL,
+                     p_retailprice DECIMAL(15,2) NOT NULL,
+                     p_comment     VARCHAR(23) NOT NULL,
+                     PRIMARY KEY (p_partkey) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE part ADD INDEX IDX_PART_PK ON (p_partkey);
 
-CREATE TABLE supplier (
-                          s_suppkey   integer        NOT NULL,
-                          s_name      varchar(25)       NOT NULL,
-                          s_address   varchar(40)    NOT NULL,
-                          s_nationkey integer        NOT NULL,
-                          s_phone     varchar(15)       NOT NULL,
-                          s_acctbal   decimal(15, 2) NOT NULL,
-                          s_comment   varchar(101)   NOT NULL,
-                          PRIMARY KEY (s_suppkey),
-                          FOREIGN KEY (s_nationkey) REFERENCES nation (n_nationkey) ON DELETE CASCADE
-);
+CREATE TABLE supplier ( s_suppkey     INTEGER NOT NULL,
+                        s_name        VARCHAR(25) NOT NULL,
+                        s_address     VARCHAR(40) NOT NULL,
+                        s_nationkey   INTEGER NOT NULL,
+                        s_phone       VARCHAR(15) NOT NULL,
+                        s_acctbal     DECIMAL(15,2) NOT NULL,
+                        s_comment     VARCHAR(101) NOT NULL,
+                        PRIMARY KEY (s_suppkey) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE supplier ADD INDEX IDX_SUPPLIER_PK ON (s_suppkey);
 
-CREATE TABLE partsupp (
-                          ps_partkey    integer        NOT NULL,
-                          ps_suppkey    integer        NOT NULL,
-                          ps_availqty   integer        NOT NULL,
-                          ps_supplycost decimal(15, 2) NOT NULL,
-                          ps_comment    varchar(199)   NOT NULL,
-                          PRIMARY KEY (ps_partkey, ps_suppkey),
-                          FOREIGN KEY (ps_partkey) REFERENCES part (p_partkey) ON DELETE CASCADE,
-                          FOREIGN KEY (ps_suppkey) REFERENCES supplier (s_suppkey) ON DELETE CASCADE
-);
+CREATE TABLE partsupp ( ps_partkey     INTEGER NOT NULL,
+                        ps_suppkey     INTEGER NOT NULL,
+                        ps_availqty    INTEGER NOT NULL,
+                        ps_supplycost  DECIMAL(15,2)  NOT NULL,
+                        ps_comment     VARCHAR(199) NOT NULL,
+                        PRIMARY KEY (ps_partkey, ps_suppkey) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE partsupp ADD INDEX IDX_PARTSUPP_PK ON (ps_partkey, ps_suppkey);
 
-CREATE TABLE customer (
-                          c_custkey    integer        NOT NULL,
-                          c_name       varchar(25)    NOT NULL,
-                          c_address    varchar(40)    NOT NULL,
-                          c_nationkey  integer        NOT NULL,
-                          c_phone      varchar(15)       NOT NULL,
-                          c_acctbal    decimal(15, 2) NOT NULL,
-                          c_mktsegment varchar(10)       NOT NULL,
-                          c_comment    varchar(117)   NOT NULL,
-                          PRIMARY KEY (c_custkey),
-                          FOREIGN KEY (c_nationkey) REFERENCES nation (n_nationkey) ON DELETE CASCADE
-);
+CREATE TABLE customer ( c_custkey     INTEGER NOT NULL,
+                        c_name        VARCHAR(25) NOT NULL,
+                        c_address     VARCHAR(40) NOT NULL,
+                        c_nationkey   INTEGER NOT NULL,
+                        c_phone       VARCHAR(15) NOT NULL,
+                        c_acctbal     DECIMAL(15,2)   NOT NULL,
+                        c_mktsegment  VARCHAR(10) NOT NULL,
+                        c_comment     VARCHAR(117) NOT NULL,
+                        PRIMARY KEY (c_custkey) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE customer ADD INDEX IDX_CUSTOMER_PK ON (c_custkey);
 
-CREATE TABLE orders (
-                        o_orderkey      integer        NOT NULL,
-                        o_custkey       integer        NOT NULL,
-                        o_orderstatus   varchar(1)        NOT NULL,
-                        o_totalprice    decimal(15, 2) NOT NULL,
-                        o_orderdate     date           NOT NULL,
-                        o_orderpriority varchar(15)       NOT NULL,
-                        o_clerk         varchar(15)       NOT NULL,
-                        o_shippriority  integer        NOT NULL,
-                        o_comment       varchar(79)    NOT NULL,
-                        PRIMARY KEY (o_orderkey),
-                        FOREIGN KEY (o_custkey) REFERENCES customer (c_custkey) ON DELETE CASCADE
-);
+CREATE TABLE orders  ( o_orderkey       INTEGER NOT NULL,
+                       o_custkey        INTEGER NOT NULL,
+                       o_orderstatus    VARCHAR(1) NOT NULL,
+                       o_totalprice     DECIMAL(15,2) NOT NULL,
+                       o_orderdate      DATE NOT NULL,
+                       o_orderpriority  VARCHAR(15) NOT NULL,
+                       o_clerk          VARCHAR(15) NOT NULL,
+                       o_shippriority   INTEGER NOT NULL,
+                       o_comment        VARCHAR(79) NOT NULL,
+                       PRIMARY KEY (o_orderkey) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE orders ADD INDEX IDX_ORDERS_PK ON (o_orderkey);
 
-CREATE TABLE lineitem (
-                          l_orderkey      integer        NOT NULL,
-                          l_partkey       integer        NOT NULL,
-                          l_suppkey       integer        NOT NULL,
-                          l_linenumber    integer        NOT NULL,
-                          l_quantity      decimal(15, 2) NOT NULL,
-                          l_extendedprice decimal(15, 2) NOT NULL,
-                          l_discount      decimal(15, 2) NOT NULL,
-                          l_tax           decimal(15, 2) NOT NULL,
-                          l_returnflag    varchar(1)        NOT NULL,
-                          l_linestatus    varchar(1)        NOT NULL,
-                          l_shipdate      date           NOT NULL,
-                          l_commitdate    date           NOT NULL,
-                          l_receiptdate   date           NOT NULL,
-                          l_shipinstruct  varchar(25)       NOT NULL,
-                          l_shipmode      varchar(10)       NOT NULL,
-                          l_comment       varchar(44)    NOT NULL,
-                          PRIMARY KEY (l_orderkey, l_linenumber),
-                          FOREIGN KEY (l_orderkey) REFERENCES orders (o_orderkey) ON DELETE CASCADE,
-                          FOREIGN KEY (l_partkey, l_suppkey) REFERENCES partsupp (ps_partkey, ps_suppkey) ON DELETE CASCADE
-);
+CREATE TABLE lineitem ( l_orderkey    INTEGER NOT NULL,
+                        l_partkey     INTEGER NOT NULL,
+                        l_suppkey     INTEGER NOT NULL,
+                        l_linenumber  INTEGER NOT NULL,
+                        l_quantity    DECIMAL(15,2) NOT NULL,
+                        l_extendedprice  DECIMAL(15,2) NOT NULL,
+                        l_discount    DECIMAL(15,2) NOT NULL,
+                        l_tax         DECIMAL(15,2) NOT NULL,
+                        l_returnflag  VARCHAR(1) NOT NULL,
+                        l_linestatus  VARCHAR(1) NOT NULL,
+                        l_shipdate    DATE NOT NULL,
+                        l_commitdate  DATE NOT NULL,
+                        l_receiptdate DATE NOT NULL,
+                        l_shipinstruct VARCHAR(25) NOT NULL,
+                        l_shipmode     VARCHAR(10) NOT NULL,
+                        l_comment      VARCHAR(44) NOT NULL,
+                        PRIMARY KEY (l_orderkey, l_linenumber) );
+-- Polypheny does not (yet) automatically create an index for the primary key (like for instance PostgreSQL)
+ALTER TABLE lineitem ADD INDEX IDX_LINEITEM_PK ON (l_orderkey, l_linenumber);
